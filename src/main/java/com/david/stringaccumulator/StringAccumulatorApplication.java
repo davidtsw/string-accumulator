784df833e3ceff940b211f80f9f794afb1054861
davidtsw <davidtangsw@yahoo.com.hk>
2019-11-27 01:59:50 +0800

    init spring boot maven project

---
+++ src/main/java/com/david/stringaccumulator/StringAccumulatorApplication.java
@@ -0,0 +1,13 @@
+package com.david.stringaccumulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringAccumulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringAccumulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davi/stringaccumulator/StringAccumulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davi.stringaccumulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringAccumulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
