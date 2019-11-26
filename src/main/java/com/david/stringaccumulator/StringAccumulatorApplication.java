f0acaf045e87ed37f88c0d22906cc7d910d6bd67
davidtsw <davidtangsw@yahoo.com.hk>
2019-11-27 03:13:45 +0800

    basic empty string test case

--- src/main/java/com/david/stringaccumulator/StringAccumulatorApplication.java
+++ src/main/java/com/david/stringaccumulator/StringAccumulatorApplication.java
@@ -1,13 +1,37 @@
 package com.david.stringaccumulator;
 
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
-public class StringAccumulatorApplication {
+@Slf4j
+public class StringAccumulatorApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StringAccumulatorApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		String numbers = "";
+		log.info("input:[{}]", numbers);
+		int sum = add(numbers);
+		log.info("total:{}", sum);
+	}
+
+	public int add(String numbers)
+	{
+		if(Strings.isBlank(numbers))
+		{
+			return 0;
+		}
+		
+		int sum = 0;
+		
+		return sum;
+	}
 }
--- src/test/java/com/david/stringaccumulator/StringAccumulatorApplicationTests.java
+++ src/test/java/com/david/stringaccumulator/StringAccumulatorApplicationTests.java
@@ -1,13 +1,20 @@
 package com.david.stringaccumulator;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class StringAccumulatorApplicationTests {
 
+	StringAccumulatorApplication app = new StringAccumulatorApplication();
+	
 	@Test
-	void contextLoads() {
+	void testEmptyString() {
+		String numbers = "";
+		int sum = app.add(numbers);
+		assertEquals(0, sum);
 	}
 
 }
